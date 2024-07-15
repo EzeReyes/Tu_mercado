45903da73e691dc91932d45887a2adf43e5d458b
ezequiel reyes <110490540+EzeReyes@users.noreply.github.com>
2024-07-15 00:19:38 -0300

    modificaciones para deploy...

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(bcrypt());
 
 
 const storage = multer.diskStorage({
